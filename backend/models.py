22b7eaa397b9979f297adcee92627bda38689224
Folayimi <ridwanfolayimi@gmail.com>
2022-07-05 05:01:54 -0700

    made the database password hidden

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-database_path = "postgresql://{}:{}@{}/{}".format('postgres','Florinfix$321','localhost:5432', database_name)
+database_password = os.environ.get('DATABASE_PASSWORD')
+database_path = "postgresql://{}:{}@{}/{}".format('postgres',database_password,'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}:{}@{}/{}".format('postgres','Florinfix$321','localhost:5432', self.database_name)
+        self.database_password = os.environ.get('DATABASE_PASSWORD')
+        self.database_path = "postgresql://{}:{}@{}/{}".format('postgres',self.database_password,'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
